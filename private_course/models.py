4dbe269a4879d83fce9bb3e291b112e36602838f
Hassan Zaki <the.pythonic@gmail.com>
2014-04-30 01:11:52 +0200

    * init(2) add members, membership

--- private_course/models.py
+++ private_course/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+COUNTRIES = ('a', 'b')
+PAYMENTS = ('1', '2')
+CATEGORIES = ('Individual', 'Group')
 
 class PrivateCourse(models.Model):
   title = models.CharField(max_length=255)
@@ -18,3 +21,22 @@ class PrivateCourse(models.Model):
     ordering = ['position']
     verbose_name = 'Private Course'
     verbose_name_plural = 'Private Courses'
+
+class MemberShip(models.Model):
+  member = models.ForeignKey('Member')
+  course = models.ForeignKey(PrivateCourse)
+  category = models.CharField(max_length=255)
+  
+class Member(models.Model):
+  name = models.CharField(max_length=255)
+  country = models.CharField(max_length=255)
+  description = models.TextField()
+  dob = models.DateField()
+  skype = models.CharField(max_length=255)
+  specialization = models.CharField(max_length=255)
+  payment = models.CharField(max_length=255)
+  courses = models.ManyToManyField(PrivateCourse, through='Membership', related_name='members')
+
+  def __unicode__(self):
+    return self.name
+
--- private_course/views.py
+++ private_course/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
+
 from .models import PrivateCourse
+from .models import Member
+from .forms import MemberForm
 
 class PrivateCourseDetailView(DetailView):
   model = PrivateCourse
@@ -8,3 +12,9 @@ class PrivateCourseDetailView(DetailView):
 class PrivateCourseListView(ListView):
   model = PrivateCourse
   queryset = PrivateCourse.objects.filter(active=True)
+
+
+class MemberCreateView(CreateView):
+  model = Member
+  form_class = MemberForm
+  success_url = reverse_lazy('home')
