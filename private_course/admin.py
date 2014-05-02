033e43918a3b7e9267647a284d8002d5abc7fdc6
Hassan Zaki <hassan.zaki@badrit.com>
2014-05-02 15:58:54 +0200

    * playing with admin interface (very powerfull indeed!)

--- private_course/admin.py
+++ private_course/admin.py
@@ -1,6 +1,21 @@
 from .models import *
 from django.contrib import admin
 
-admin.site.register(PrivateCourse)
-admin.site.register(Member)
-admin.site.register(Membership)
+
+class PrivateCourseAdmin(admin.ModelAdmin):
+  list_display = ('title', 'description', 'position', 'active', 'capacity', 'hours')
+  list_editable = ('position', 'active', 'capacity', 'hours')
+  list_filter = ('active',)
+  # list_select_related = ('members',)
+  ordering = ('position', 'capacity')
+  search_fields = ('title',)
+
+class MemberAdmin(admin.ModelAdmin):
+    pass
+
+class MembershipAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PrivateCourse, PrivateCourseAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Membership, MembershipAdmin)
